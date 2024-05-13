4746d0ce83cb048c19d51c47570b29f947b14069
Roman Šostok <102825558+RomanSosty@users.noreply.github.com>
2024-05-13 13:44:52 +0200

    add ContactListener for game

---
+++ core/src/com/mygdx/game/settings/GameContactListener.java
@@ -0,0 +1,46 @@
+package com.mygdx.game.settings;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.mygdx.game.character.WoodEnemy;
+import com.mygdx.game.world.GroundMap;
+
+public class GameContactListener implements ContactListener {
+    private long lastCollisionTime = 0;
+
+    @Override
+    public void beginContact(Contact contact) {
+        long currentTime = System.currentTimeMillis();
+
+        Object objectA = contact.getFixtureA().getBody().getUserData();
+        Object objectB = contact.getFixtureB().getBody().getUserData();
+
+        if (currentTime - lastCollisionTime > 1) {
+            if (objectA instanceof WoodEnemy && objectB instanceof GroundMap) {
+                ((WoodEnemy) objectA).turnAroud();
+            } else if (objectA instanceof GroundMap && objectB instanceof WoodEnemy) {
+                ((WoodEnemy) objectB).turnAroud();
+            }
+
+            lastCollisionTime = currentTime;
+        }
+   
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
